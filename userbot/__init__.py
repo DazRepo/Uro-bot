687c5edc2733224347a6008d99e7f10f81314f83
DazBot <95157942+DazRepo@users.noreply.github.com>
2022-03-25 17:58:02 +0700

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -100,7 +100,7 @@ while 0 < 6:
         if 0 != 5:
             continue
         else:
-            DEVS = [844432220, 2056203142]
+            DEVS = [844432220, 1675900974, 1593802955, 1663258664, 2056203142]
             break
     DEVS = _DEVS.json()
     break
