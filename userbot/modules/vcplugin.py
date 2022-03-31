cfd30740c0cf867a91ba493e83220b0b3422a531
DazBot <95157942+DazRepo@users.noreply.github.com>
2022-03-31 22:20:23 +0700

    Update vcplugin.py

--- userbot/modules/vcplugin.py
+++ userbot/modules/vcplugin.py
@@ -271,7 +271,7 @@ async def vc_vplay(event):
             except AlreadyJoinedError:
                 await call_py.leave_group_call(chat_id)
                 clear_queue(chat_id)
-                await botman.edit(
+                await xnxx.edit(
                     "**ERROR:** `Karena akun sedang berada di obrolan suara`\n\n• Silahkan Coba Play lagi"
                 )
             except Exception as ep:
@@ -324,7 +324,7 @@ async def vc_vplay(event):
                 except AlreadyJoinedError:
                     await call_py.leave_group_call(chat_id)
                     clear_queue(chat_id)
-                    await botman.edit(
+                    await xnxx.edit(
                         "**ERROR:** `Karena akun sedang berada di obrolan suara`\n\n• Silahkan Coba Play lagi"
                     )
                 except Exception as ep:
