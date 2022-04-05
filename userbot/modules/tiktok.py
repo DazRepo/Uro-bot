34bd0cf962f7e9aabbf468d83a49bdc428d4af6c
DazBot <95157942+DazRepo@users.noreply.github.com>
2022-04-05 11:16:09 +0700

    Update tiktok.py

--- userbot/modules/tiktok.py
+++ userbot/modules/tiktok.py
@@ -7,7 +7,7 @@ from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.functions.contacts import UnblockRequest
 from telethon import events
 from userbot import CMD_HANDLER as cmd
-from userbot import CMD_HELP
+from userbot import CMD_HELP, 
 from userbot.utils import edit_delete, edit_or_reply, poci_cmd
 
 
@@ -56,48 +56,41 @@ async def insta(event):
 
 @poci_cmd(pattern="tiktok(?: |$)(.*)")
 async def _(event):
-    xxnx = event.pattern_match.group(1)
-    if xxnx:
-        d_link = xxnx
-    elif event.is_reply:
-        d_link = await event.get_reply_message()
-    else:
-        return await edit_delete(
-            event,
-            "**Berikan Link Tiktok Pesan atau Reply Link Tiktok Untuk di Download**",
+    if event.fwd_from:
+        return
+    d_link = event.pattern_match.group(1)
+    if ".com" not in d_link:
+        await event.edit(
+            "`Mohon Maaf, Saya Membutuhkan Link Video Tiktok Untuk Mendownload Nya`"
         )
-    xx = await edit_or_reply(event, "`Video Sedang Diproses...`")
-    chat = "@thisvidbot"
-    async with event.client.conversation(chat) as conv:
+    else:
+        await event.edit("```Video Sedang Diproses.....```")
+    chat = "@ttsavebot"
+    async with bot.conversation(chat) as conv:
         try:
             msg_start = await conv.send_message("/start")
             r = await conv.get_response()
             msg = await conv.send_message(d_link)
             details = await conv.get_response()
             video = await conv.get_response()
-            text = await conv.get_response()
-            await event.client.send_read_acknowledge(conv.chat_id)
+            """ - don't spam notif - """
+            await bot.send_read_acknowledge(conv.chat_id)
         except YouBlockedUserError:
-            await event.client(UnblockRequest(chat))
-            msg_start = await conv.send_message("/start")
-            r = await conv.get_response()
-            msg = await conv.send_message(d_link)
-            details = await conv.get_response()
-            video = await conv.get_response()
-            text = await conv.get_response()
-            await event.client.send_read_acknowledge(conv.chat_id)
-        await event.client.send_file(event.chat_id, video)
+            await event.edit(
+                "**Kesalahan:** `Mohon Buka Blokir` @ttsavebot `Dan Coba Lagi !`"
+            )
+            return
+        await bot.send_file(event.chat_id, video)
         await event.client.delete_messages(
-            conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id, text.id]
+            conv.chat_id, [msg_start.id, r.id, msg.id, details.id, video.id]
         )
-        await xx.delete()
-            
+        await event.delete()
+
+
 CMD_HELP.update(
     {
-        "tiktok": f"**Plugin : **`tiktok`\
-        \n\n  •  **Syntax :** `{cmd}tiktok` <link>\
-        \n  •  **Function : **Download Video Tiktok Tanpa Watermark\
-    "
+        f"tiktok": "𝘾𝙤𝙢𝙢𝙖𝙣𝙙: `{cmd}tiktok <Link tiktok>`"
+        "\n• : Download Video Tiktok Tanpa Watermark"
     }
 )
 
