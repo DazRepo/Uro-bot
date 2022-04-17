e980e4c1d166f79c733ce379afd5dc01c51d3c77
DazBot <95157942+DazRepo@users.noreply.github.com>
2022-04-17 12:46:51 +0700

    Update carbon.py

--- userbot/modules/carbon.py
+++ userbot/modules/carbon.py
@@ -10,6 +10,7 @@ import random
 from userbot import CMD_HANDLER as cmd
 from userbot import CMD_HELP
 from userbot.utils import edit_delete, edit_or_reply, poci_cmd
+from userbot.utils.misc import Carbon
 
 from .vcplugin import vcmention
 
